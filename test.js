3cb211d27eaa832c0a2167199c3f085dab9619c4
Soldy <4786022+Soldy@users.noreply.github.com>
2022-04-17 18:08:47 +0100

    test update
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- test.js
+++ test.js
@@ -1,6 +1,36 @@
-const sleep = require('./index.js').sleep;
-(async ()=>{
-    console.log('stand by for 3 seconds');
-    await sleep(3);
-    console.log('that was 3 seconds');
-})();
+#!/usr/bin/nodejs
+'use strict';
+
+const $test  = new (require('nanoTest')).test({
+    'progress_bar' : false,
+    'debug_print' : 'short'
+});
+
+const $sleep = require('./index.js').sleep;
+
+
+const _start_time = parseInt(Date.now());
+
+$test.add(
+    'sleep 3 sec',
+    {
+        'function': $sleep,
+        'options' :[3]
+    },
+    '!error'
+);
+$test.add(
+    'time check',
+    {
+        'function': function(){
+             return Math.round(
+                 ((Date.now())-_start_time)/1000
+             );
+        },
+        'options' :[]
+    },
+    '===',
+    3
+);
+
+$test.run();
